11dc543d30fdf65fef6391d7f36cdd374212fb87
Muhammad Fathan <fathnakbar@gmail.com>
2024-08-16 08:49:01 +0700

    init nuxt project

---
+++ pages/early-adopter.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="min-h-screen bg-gray-100 flex flex-col justify-center items-center text-center">
+      <header class="text-2xl font-bold mb-6">
+        Expose - Exporteer Operating System Enterprise
+      </header>
+  
+      <section class="text-lg mb-4 px-6">
+        Bergabung bersama early adopter yang lain
+      </section>
+  
+      <div class="text-2xl font-bold text-red-600 mb-8">
+        Sisa Kuota 93 dari 1000
+      </div>
+  
+      <button 
+        @click="register"
+        :class="{
+          'bg-blue-500 hover:bg-blue-700 text-white': remainingQuota > 0,
+          'bg-gray-400 text-gray-200': remainingQuota <= 0
+        }"
+        class="px-6 py-3 text-lg rounded-md focus:outline-none"
+        :disabled="remainingQuota <= 0"
+      >
+        Register Now
+      </button>
+    </div>
+  </template>
+  
+  <script setup>
+  import { ref } from 'vue';
+  
+  const remainingQuota = ref(50);
+  
+  const register = () => {
+    window.location.href = "https://whatsform.com/hhF8Mx"
+  };
+  </script>
+  
+  <style scoped>
+  /* Tailwind handles most of the styling. No additional styles needed here. */
+  </style>
+  
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <UContainer>
+    <UCard class="mt-10">
+      <template #header>
+        <div class="flex justify-between">
+          <h1>Welcome to Nuxt UI Starter</h1>
+        </div>
+      </template>
+      <UButton icon="i-heroicons-book-open" to="https://ui.nuxt.com" target="_blank">Open Nuxt UI Documentation</UButton>
+    </UCard>
+  </UContainer>
+</template>
